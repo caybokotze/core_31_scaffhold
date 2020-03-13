d20426b5666a6b8d2d54b628cdaa657e68ddd775
Jack <kotzecabo@gmail.com>
2020-03-13 15:53:27 +0200

    First Commit

---
+++ CleanScaffholdWithAuth/Controllers/WeatherController.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using CleanScaffholdWithAuth.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanScaffholdWithAuth.Controllers
+{
+    public class WeatherController : Controller
+    {
+        private ApplicationDbContext _context;
+        public WeatherController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET
+        public IActionResult Index()
+        {
+            return Ok(_context.Users.ToList());
+        }
+    }
+}
